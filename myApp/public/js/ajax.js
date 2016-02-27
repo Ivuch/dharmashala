20c4ef6a27c70152109047003b60caf703a66b56
ivuch <ivan.matellan@hotmail.com>
2016-02-27 04:42:13 -0300

    Buen refactor!

--- myApp/public/js/ajax.js
+++ myApp/public/js/ajax.js
@@ -9,92 +9,94 @@ function getNewXHRObject(){
   return xhr;
 }
 
+/** 
+LOGIN function():
+  Si el status = 200 inicia 
+  El Content-type Negociation: 
+        Si es "application/json" -->login con error (se setea la clase .error de CSS)
+        Si es cualquier otro  -->> se hace un render al xhr.responseText (debería ser un HTML)
+**/
 function login(){
-  var xhr = getNewXHRObject()
   var url = baseURL+"/login"
   var user = document.getElementById("loginForm").elements["user"].value
   var pass = document.getElementById("loginForm").elements["password"].value
   var params = "user="+user+"&password="+pass
+  var xhr = getNewXHRObject()
+  xhr.open("POST", url, true)
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(params)
   xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE ) {
-           if(xhr.status == 200){
-             var content = xhr.getResponseHeader("Content-Type")
-             if(content == "application/json; charset=utf-8"){
-                var json = JSON.parse(xhr.responseText)
-              if(json.isERROR){
-                document.getElementById("loginForm").classList.add("error")
-               }
-             }else{
-                  document.open()
-                  document.write(xhr.responseText)
-                  document.close()
-              }
-              
-           }
-           else if(xhr.status == 400) {
-              alert('There was an error 400')
-           }
-           else {
-               alert('something else other than 200 was returned')
-           }
-        }
+    if (xhr.readyState == XMLHttpRequest.DONE ) {
+       if(xhr.status == 200){
+          var content = xhr.getResponseHeader("Content-Type")
+          if(content == "application/json; charset=utf-8"){
+            var json = JSON.parse(xhr.responseText)
+            if(json.isERROR){
+              document.getElementById("loginForm").classList.add("error")
+            }
+          }else{
+            document.open()
+            document.write(xhr.responseText)
+            document.close()
+          }  
+        }else if(xhr.status == 400) {
+          alert('There was an error 400')
+        }else {
+          alert('something else other than 200 was returned')
+      }
     }
-
-    xhr.open("POST", url, true)
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(params)
+  }
 }
 
+/** Document.onLoad(callback)**/
 document.addEventListener("DOMContentLoaded",function(){
   var url = baseURL+"/text"
   var xhr = getNewXHRObject();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE ) {
-           if(xhr.status == 200){
-              var json = JSON.parse(xhr.responseText)
-              var text= json.user.conversations[0].messages[0].text
-               document.getElementById("chatContent").innerHTML = text;
-           }
-           else if(xhr.status == 400) {
-              alert('There was an error 400')
-           }
-           else {
-               alert('something else other than 200 was returned')
-           }
-        }
-    }
-
-    xhr.open("GET", url, true)
-    xhr.send(null)
-
+  xhr.open("GET", url, true)
+  xhr.send(null)
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE ) {
+         if(xhr.status == 200){
+            var json = JSON.parse(xhr.responseText)
+            var text= json.user.conversations[0].messages[0].text
+             document.getElementById("chatContent").innerHTML = text;
+         }
+         else if(xhr.status == 400) {
+            alert('There was an error 400')
+         }
+         else {
+             alert('something else other than 200 was returned')
+         }
+      }
+  }
 })
 
+/**  AJAX Request Template**/
 function ajaxReq(){
+  var url = baseURL+"/text"
 	var text = document.getElementById("chatForm").elements["text"].value
-	var urlPost = baseURL+"/text"
 	var params = "text="+text
   var xhr = getNewXHRObject()
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE ) {
-           if(xhr.status == 200){
-           		var json = JSON.parse(xhr.responseText)
-              var chat = document.getElementById("chatContent")
-              chat.innerHTML = chat.innerHTML+'<br>'+ json.text;
-              updateScroll()
-              document.getElementById("chatForm").elements["text"].value = ""
-           }
-           else if(xhr.status == 400) {
-              alert('There was an error 400')
-           }
-           else {
-               alert('something else other than 200 was returned')
-           }
-        }
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(params);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE ) {
+     if(xhr.status == 200){
+     		var json = JSON.parse(xhr.responseText)
+        var chat = document.getElementById("chatContent")
+        chat.innerHTML = chat.innerHTML+'<br>'+ json.text;
+        updateScroll()
+        document.getElementById("chatForm").elements["text"].value = ""
+      }
+      else if(xhr.status == 400) {
+        alert('There was an error 400')
+      }
+      else {
+         alert('something else other than 200 was returned')
+      }
     }
-
-    xhr.open("POST", urlPost, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(params);
+  }
 }
 
 var scrolled = false;
